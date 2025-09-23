7b147c2cf2f1de97481e036f847f4eebe05f9b3c
pebencze <petra.bncz@gmail.com>
2025-09-23 17:04:38 +0200

    refactor(dashboard/index.js): move elements to top level

--- javascript/async/dashboard/index.js
+++ javascript/async/dashboard/index.js
@@ -1,5 +1,10 @@
+const weatherEl = document.getElementById("weather");
+const authorEl = document.getElementById("author");
+const cryptoTopEl = document.getElementById("crypto-top");
+const cryptoEl = document.getElementById("crypto");
+const timeEl = document.getElementById("time");
+
 function setBackground() {
-    const author = document.getElementById("author");
     fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(response => {
         if (!response.ok) {
@@ -10,13 +15,13 @@ function setBackground() {
     .then(data => {
         //console.log(data);
         document.body.style.backgroundImage = `url(${data.urls.full})`;
-        author.textContent = `by ${data.user.name}`;
+        authorEl.textContent = `by ${data.user.name}`;
     })
     .catch(error => {
         console.error("Error fetching the background image:", error);
         const defaultImageUrl = 'https://images.unsplash.com/photo-1470240731273-7821a6eeb6bd?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NTg1NDUxNTB8&ixlib=rb-4.1.0&q=85';
         document.body.style.backgroundImage = `url(${defaultImageUrl})`;
-        author.textContent = `by Unknown Author`;
+        authorEl.textContent = `by Unknown Author`;
     })
 };
 
@@ -29,10 +34,10 @@ function getCoinData() {
         return response.json()
     })
     .then(data => {
-        const coinEl = document.createElement("p")
+        const coinEl = document.createElement("p");
         coinEl.innerHTML = `<img src=${data.image.small}/> <span>${data.name}</span>`
-        document.getElementById("crypto-top").appendChild(coinEl);
-        document.getElementById("crypto").innerHTML += ` 
+        cryptoTopEl.appendChild(coinEl);
+        cryptoEl.innerHTML += ` 
                     <p>Current price: $${data.market_data.current_price.usd}</p> 
                     <p>High price:    $${data.market_data.high_24h.usd}</p> 
                     <p>Low price:     $${data.market_data.low_24h.usd}</p> 
@@ -46,7 +51,7 @@ function getCoinData() {
 
 function getCurrentTime () {
     const date = new Date();
-    document.getElementById("time").textContent = date.toLocaleTimeString("en-US", {timeStyle: "medium"});
+    timeEl.textContent = date.toLocaleTimeString("en-US", {timeStyle: "medium"});
 }
 
 // // this is an older version callback-function
@@ -75,7 +80,6 @@ function getWeather(lat, lon){
         return response.json()
     })
     .then(data => {
-        const weatherEl = document.getElementById("weather");
         const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
         weatherEl.innerHTML = `
             <img src=${iconUrl} />
@@ -85,7 +89,7 @@ function getWeather(lat, lon){
     })
     .catch(error => {
         console.error(error);
-        document.getElementById("weather").textContent = "Weather data unavailable";
+        weatherEl.textContent = "Weather data unavailable";
     });
 
 }
@@ -95,10 +99,9 @@ getCoinData();
 setInterval(getCurrentTime, 1000); // calls the function every 1000ms
 getGeolocationWithPromise()
     .then((position) => {
-        console.log(position);
         getWeather(position.coords.latitude, position.coords.longitude);
     })
     .catch((error) => {
         console.error(error);
-        document.getElementById("weather").textContent = "Location data unavailable";
+        weatherEl.textContent = "Location data unavailable";
     });
