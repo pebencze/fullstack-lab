a2f0c7e55f392c05efe401a5d54eaad4c2467e83
pebencze <petra.bncz@gmail.com>
2025-09-24 19:09:03 +0200

    feat(react): create functioning  tic-tac-toe game

--- react/tic-tac-toe/src/App.jsx
+++ react/tic-tac-toe/src/App.jsx
@@ -1,36 +1,92 @@
+import { useState } from "react"
 
 /**
- * Default function of our module
+ * Parent Board component; default function of our module
+ * @detail each row contains 3 Square elements that take a value prop;
+ * the setSquares function triggers re-rendering of components that use the squares state
  * @returns a fragment denoted by <> and </>
  */
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  /**
+   * 
+   * @param nextSquares uses immutability -> we copy the original array's content
+   * by using .slice() and so we create nextSquares array; this way, we don't have to change 
+   * the parent array and it has benefits: re-rendering will only happen to the changed indexes,
+   * not to the whole array; we can roll back or save a previous state
+   */
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
       </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+      <div className="board-row"> 
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/> 
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
   );
 }
 
 /**
- * 
+ * Child Square component
  * @param value: square component can be passed a _prop_ called "value";
  * when using it, add curly braces `{value}` to escape from JSX to JavaScript
  * @returns Square component, a child component of Board
  */
-function Square({value}) { 
-  return <button className="square">{value}</button>;
+function Square({value, onSquareClick}) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
