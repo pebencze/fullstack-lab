5dd6776c251dcaa1a0b6f258057a049b31e8b4ee
pebencze <petra.bncz@gmail.com>
2025-09-26 12:01:17 +0200

    feat(react/data-driven-react): add props

--- react/data-driven-react/src/App.jsx
+++ react/data-driven-react/src/App.jsx
@@ -2,43 +2,55 @@ import Header from '@/components/Header';
 import Entry from './components/Entry';
 import Joke from './components/Joke';
 
-// function App(){
-//   return (
-//     <>
-//       <Header />
-//       <Entry />
-//     </>
-//   )
-// }
-
 function App(){
   return (
     <>
-      <Joke
-        punchline="I can't wait to see her face light up when she opens it."
-        upVotes={0}
-        isPun={true}
-      />
-      <Joke
-        setup="How did the hacker escape the police?"
-        punchline="I can't wait to see her face light up when she opens it."
-        upVotes={6}
-        isPun={true}
-      />
-      <Joke
-        setup="I got my daughter a fridge for her birthday."
-        punchline="He just ransomware!"
-        upVotes={10}
-        isPun={true}
-      />
-      <Joke
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is a big plus!"
-        upVotes={2}
-        isPun={false}
-      />
+      <Header />
+      <main className="container">
+        <Entry 
+          img={{
+              src: "https://upload.wikimedia.org/wikipedia/commons/f/f8/View_of_Mount_Fuji_from_%C5%8Cwakudani_20211202.jpg",
+              alt: "Mount Fuji"
+          }}
+          title="Mount Fuji"
+          country="Japan"
+          googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+          dates="12 Jan, 2021 - 24 Jan, 2021"
+          description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        />
+      </main>
     </>
   )
 }
+
+// function App(){
+//   return (
+//     <>
+//       <Joke
+//         punchline="I can't wait to see her face light up when she opens it."
+//         upVotes={0}
+//         isPun={true}
+//       />
+//       <Joke
+//         setup="How did the hacker escape the police?"
+//         punchline="I can't wait to see her face light up when she opens it."
+//         upVotes={6}
+//         isPun={true}
+//       />
+//       <Joke
+//         setup="I got my daughter a fridge for her birthday."
+//         punchline="He just ransomware!"
+//         upVotes={10}
+//         isPun={true}
+//       />
+//       <Joke
+//         setup="What's the best thing about Switzerland?"
+//         punchline="I don't know, but the flag is a big plus!"
+//         upVotes={2}
+//         isPun={false}
+//       />
+//     </>
+//   )
+// }
 
 export default App;
--- react/data-driven-react/src/components/Entry.jsx
+++ react/data-driven-react/src/components/Entry.jsx
@@ -1,22 +1,16 @@
-export default function Entry() {
+export default function Entry(props) {
     return (
         <article className="journal-entry">
             <div className="location-image-container">
-                <img className="location-image" src="https://upload.wikimedia.org/wikipedia/commons/f/f8/View_of_Mount_Fuji_from_%C5%8Cwakudani_20211202.jpg" alt="location"/>
+                <img className="location-image" src={props.img.src} alt={props.img.alt}/>
             </div>
             <div className="info-container">
                 <img className="marker-image" src="./src/assets/marker.png" alt="tiny marker"/>
-                <span className="country">Japan</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/
-                @35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a
-                42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.
-                7273634!16zL20vMGNrczA?entry=ttu">View on Google maps</a>
-                <h1>Mount Fuji</h1>
-                <p className="dates">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="description">
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). 
-                    Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
-                </p>
+                <span className="country">{props.country}</span>
+                <a href={props.googleMapsLink}>View on Google maps</a>
+                <h1>{props.title}</h1>
+                <p className="dates">{props.dates}</p>
+                <p className="description">{props.description}</p>
             </div>
         </article>
     )
