80c258ea85af530ef2ba15b2483975a471bde059
pebencze <petra.bncz@gmail.com>
2025-09-29 18:37:58 +0200

    refactor(react/chef-claude): extract sections to components, use shared state

---
+++ react/chef-claude/src/components/IngredientsList.jsx
@@ -0,0 +1,17 @@
+
+
+export default function IngredientsList(props){
+    return (
+        <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{props.ingredientsList}</ul>
+                {props.ingredients.length >= 4 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button onClick={props.toggleRecipeShown}>Get a recipe</button>
+                </div>}
+            </section>
+    )
+}
